671ce413db7c6b981d1ed009810ff72f766800f6
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-08-21 19:13:47 -0300

    Change server port from 3000 to 8080 in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
