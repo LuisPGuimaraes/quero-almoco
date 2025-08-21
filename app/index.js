290dfc9d00647ee97e17cf4e0b8c9ada9c71bdba
LuisPGuimaraes <luisegsilva.le@gmail.com>
2025-08-21 20:35:08 -0300

    Add server functionality in app/index.js and implement form submission in submitForm.js; remove old index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const submitForm = require('../submitForm');
+const submitForm = require('./submitForm');
 require('dotenv').config();
 
 const app = express();
